434d881f5b02a026b672ff1fddca7ab02f08284c
mayumi <mayumi@asadigital.net>
2019-09-17 22:55:41 +0800

    ニュースのレイアウト調整・ニュースのseeder作成・app.js復活

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,12 +14,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/layout.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
 
     @include('._script')
 
-    <title>AmaArbaro @yield('title')</title>
+    <title>@yield('title') | AmaArbaro</title>
     </head>
     <body>
         {{-- toast --}}
